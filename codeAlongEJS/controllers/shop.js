2de1dbc31eb180b762d255a70b87065ecec5e647
rbridge1104 <35268078+rbridge1104@users.noreply.github.com>
2022-01-27 20:23:17 -0500

    catching desktop up

---
+++ codeAlongEJS/controllers/shop.js
@@ -0,0 +1,45 @@
+const res = require('express/lib/response');
+const Product = require('../models/product');
+
+
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Items',
+      path: '/products'
+    });
+  });
+};
+
+exports.getIndex = (req,res, next)  => {
+  Product.fetchAll((products) => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle:'Your Cart'
+  })
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle:'Orders'
+  })
+};
+
+exports.getCheckout = (req,res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  })
+}
--- codeAlongEJS/models/product.js
+++ codeAlongEJS/models/product.js
@@ -1,43 +1,33 @@
 const fs = require('fs');
 const path = require('path');
-
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+const getProductsFromFile = (cb) => {
+	fs.readFile(p, (err, fileContent) => {
+		if (err) {
+			cb([]);
+		} else {
+			cb(JSON.parse(fileContent));
+		}
+	});
+};
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
-    save() {
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
-        });
+	constructor(title, imageUrl, description, price) {
+		this.title = title;
+		this.imageUrl= imageUrl;
+		this.description = description;
+		this.price = price;
+	}
+	save() {
+		getProductsFromFile((products) => {
+			products.push(this);
+			fs.writeFile(p, JSON.stringify(products), (err) => {
+				console.log(err);
+			});
+		});
+		fs.readFile(p, (err, fileContent) => {});
+	}
 
-    }
-
-    static fetchAll(cb) {
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([])
-            }
-            cb(JSON.parse(fileContent));
-        });
-
-    }
-
-
+	static fetchAll(cb) {
+		getProductsFromFile(cb);
+	}
 };
